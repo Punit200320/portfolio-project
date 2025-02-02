0c4a26400feebd7670a32d53fba95486b5db90fd
Punit200320 <147536292+Punit200320@users.noreply.github.com>
2025-02-02 12:41:59 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Add a scroll event listener to the window
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
